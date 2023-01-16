332623211b3d4b8f411f5f8a3159da55e6673476
Gavin <gavinwalsh739@gmail.com>
2023-01-16 16:23:21 +0100

    Refactored navbar to make it resuable and easier to style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import "@fontsource/syne"
+import Logo from './images/logo.png'
 
 function App() {
+
+  //Links & Logo
+  const [links, setLinks] = useState(['HOME', 'FEATURES', 'PAGES', 'PORTFOLIO', 'BLOG', 'SHOP', 'SHORTCODES']);
+  const [logo, setLogo] = useState({Image: Logo})
 
   return (
 
     <div>
-      <Navbar />
+      <Navbar links={links} logo={logo}/>
     </div>
 
   )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,9 @@
 import React, { useState } from "react";
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
-import Logo from '../images/logo.png'
 
-function Navbar() {
+function Navbar({links, logo}) {
 
-    //test
 
     // State to open and close nav bar on mobile
   const [isOpen, setIsOpen] = useState(false);
@@ -15,7 +13,7 @@ function Navbar() {
     <nav className={`bg-slate-500 text-white py-3 px-5 flex justify-around items-end flex-col lg:flex-row`}>
 
         {/* Logo */}
-        <span className="lg:block hidden cursor-pointer"><img src={Logo}></img></span>
+        <span className="lg:block hidden cursor-pointer"><img src={logo.Image}></img></span>
 
         {/* Button for mobile nav */}
       <button className="block lg:hidden" onClick={() => setIsOpen(!isOpen)}>
@@ -29,13 +27,13 @@ function Navbar() {
         {/* Links */}
       <div className={`${ isOpen ? "block" : "hidden" } lg:flex lg:items-center lg:w-auto h-screen lg:h-16 `} >
         <div className="flex flex-col text-sm lg:flex-grow lg:flex-row gap-12 sm:p-0 p-5">
-            <span>HOME</span>
-            <span>FEATURES</span>
-            <span>PAGES</span>
-            <span>PORTFOLIO</span>
-            <span>BLOG</span>
-            <span>SHOP</span>
-            <span>SHORTCODES</span>
+
+            {links.map((link) => (
+             <>
+                <span className="z cursor-pointer">{link}</span>
+            </>
+            ))}
+
         </div>
       </div>
 
